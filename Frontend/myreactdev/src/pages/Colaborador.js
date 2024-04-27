7ce4b259dda39a8219e8e9fb34b0302600e2ca33
ManuelPachecoUAB <127448910+ManuelPachecoUAB@users.noreply.github.com>
2024-04-27 20:03:26 +0200

    Improve menu pagina coloraborador

--- Frontend/myreactdev/src/pages/Colaborador.js
+++ Frontend/myreactdev/src/pages/Colaborador.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import NavbarColaborador from '../components/NavbarColaborador';
 import '../estilos/Welcome.css'; //
 
 export default function Colaborador() {
     return (
         <div className="main-container">
             <div>
-                <Navbar />
+                <NavbarColaborador />
                 <div className="welcome-section">
                     <img src="/Frontend/myreactdev/public/img/logo.png" alt="Welcome" className="welcome-image" />
                     <h1 className="welcome-text">Bem-vindo ao Flex2Work</h1>
