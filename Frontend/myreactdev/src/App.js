f57c3d89bd77aab61d4c5af12833da3b4838c7d0
ManuelPachecoUAB <127448910+ManuelPachecoUAB@users.noreply.github.com>
2024-05-27 21:44:40 +0200

    Adicionar ProtectedRoute4.js RHManager.js

--- Frontend/myreactdev/src/App.js
+++ Frontend/myreactdev/src/App.js
@@ -9,11 +9,13 @@ import RegisterPage from './pages/RegisterPage'
 import Colaborador from './pages/Colaborador'
 import Manager from './pages/Manager'
 import RH from './pages/RH'
+import RHManager from './pages/RHManager'
 import Admin from './pages/Admin'
 import Logout from './pages/Logout'
 import ProtectedRoute1 from './components/ProtectedRoute1';
 import ProtectedRoute2 from './components/ProtectedRoute2';
 import ProtectedRoute3 from './components/ProtectedRoute3';
+import ProtectedRoute4 from './components/ProtectedRoute4';
 import ProtectedRoute5 from './components/ProtectedRoute5';
 
  
@@ -38,6 +40,11 @@ function App() {
                   <ProtectedRoute3>
                       <RH />
                   </ProtectedRoute3>
+              } />
+              <Route path="/rhmanager" element={
+                  <ProtectedRoute4>
+                      <RHManager />
+                  </ProtectedRoute4>
               } />
               <Route path="/admin" element={
                   <ProtectedRoute5>
