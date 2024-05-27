69eb95ca5de1ef524e89ca56c98dfa849bf34e7d
ManuelPachecoUAB <127448910+ManuelPachecoUAB@users.noreply.github.com>
2024-05-27 21:46:12 +0200

    Melhorar navegacao com protecao para os diferentes tipos de utilizadores apenas verem o necessario

---
+++ Frontend/myreactdev/src/components/NavbarRHManager.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link,useNavigate } from 'react-router-dom';
+import '../estilos/NavbarLogado.css';
+import axios from "axios"; // Make sure the navbar styles are located here
+
+function NavbarRHManager() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // Call the backend logout endpoint
+        axios.get('http://127.0.0.1:5000/logout') // Assuming the backend is proxied correctly
+            .then(response => {
+                sessionStorage.removeItem('userToken');
+                localStorage.removeItem('userToken');
+                navigate('/login');
+            })
+            .catch(error => {
+                console.error('Logout failed', error);
+            });
+    };
+
+    return (
+        <nav className="NavbarRHManager">
+            <div className="logo-container">
+                <Link to="/" className="navbar-logo">
+                    <img src={'/img/logo.png'} alt="Flex2Work" />
+                </Link>
+            </div>
+            <ul className="nav-menu">
+                <li className="nav-item">
+                    <Link to="/Colaborador" className="nav-link">Colaborador</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/Manager" className="nav-link">Manager</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/Rh" className="nav-link">RH</Link>
+                </li>
+                <li className="nav-item">
+                    <button onClick={handleLogout} className="nav-link">LOGOUT</button>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavbarRHManager;
