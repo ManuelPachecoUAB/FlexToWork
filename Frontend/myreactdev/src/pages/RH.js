2cf9bebc0ee7e1c10f127005ef415b1889d17cea
2101625 <2101625@estudante.uab.pt>
2024-06-02 21:42:30 +0100

    axios utilizado para os endpoints. NavBar atualizada apra o NavBarlogado para manter consistência com outras páginas. variáveis para manipulação da visualização criados. Estes são atualizados de acordo com os eventos do utilizador, ao clicar em certos botões. Estes são atualizados e fazem uso do useEffect para executarem as funções específicas de atualização dos dados. fetchAllUserEvents faz chamada ao backend para obter os dados de todos os eventos dos utilizadores. fetchTeams faz chamada ao backend para obter as equipas existentes na DB formatEvents faz a formatação específica a ser msotrada para cada um dos eventos. handleMonthChange - dependendo do botão que é clicado, altera a data das variáveis para o mês anterior ou posterior, despoletando também o trigger do useEffect mencionado anteriormente. handleTeamChange - mesma lógica da anterior, mas a diferença está no facto de ser uma alteração de quipa (seleção em dropdown list). generateCSVReport permite a criação de um report de dados formatado com o mapa retornado pelo backend com um header com a descrição de cada uma das colunas e dependendo se foi selecionado o export para determinado mês ou para determinado ano, sendo também possível filtrar por equipa. A formatação escolhida foi UTF-8 com separação ;

--- Frontend/myreactdev/src/pages/RH.js
+++ Frontend/myreactdev/src/pages/RH.js
@@ -1,157 +1,241 @@
-import React, { useState } from "react";
-import { Link } from 'react-router-dom';
-import NavbarRH from '../components/NavbarLogado.js';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Navbarlogado from '../components/NavbarLogado.js';
 import '../estilos/NavbarLogado.css';
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import '../estilos/RH.css'; // Certifique-se de criar esse arquivo para os estilos
 
 export default function RH() {
-    const [selectedDate, setSelectedDate] = useState(null);
-    const [eventName, setEventName] = useState("");
-    const [events, setEvents] = useState([]);
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [allUserEvents, setAllUserEvents] = useState([]);
+    const [formattedEvents, setFormattedEvents] = useState([]);
+    const [currentMonth, setCurrentMonth] = useState(selectedDate.getMonth());
+    const [currentYear, setCurrentYear] = useState(selectedDate.getFullYear());
+    const [selectedTeam, setSelectedTeam] = useState('');
+    const [teams, setTeams] = useState([]);
 
-    const Date_Click_Fun = (date) => {
-        setSelectedDate(date);
-    };
+    useEffect(() => {
+        fetchAllUserEvents();
+        fetchTeams();
+    }, [currentMonth, currentYear, selectedTeam]);
 
-    const Event_Data_Update = (event) => {
-        setEventName(event.target.value);
-    };
-
-    const Create_Event_Fun = () => {
-        if (selectedDate && eventName) {
-            const newEvent = {
-                id: new Date().getTime(),
-                date: selectedDate,
-                title: eventName,
-            };
-            setEvents([...events, newEvent]);
-            setSelectedDate(null);
-            setEventName("");
-            setSelectedDate(newEvent.date);
+    const fetchAllUserEvents = async () => {
+        try {
+            const token = localStorage.getItem('userToken');
+            if (!token) {
+                console.error("Token JWT não encontrado");
+                return;
+            }
+            let response;
+            if (selectedTeam) {
+                response = await axios.get(`http://localhost:5000/api/all_users_events_byteam/${selectedTeam}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+            } else {
+                response = await axios.get('http://localhost:5000/api/all_users_events', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+            }
+            setAllUserEvents(response.data);
+            formatEvents(response.data);
+        } catch (error) {
+            console.error("Erro ao obter eventos de todos os usuários:", error.response ? error.response.data : error.message);
         }
     };
 
-    const Update_Event_Fun = (eventId, newName) => {
-        const updated_Events = events.map((event) => {
-            if (event.id === eventId) {
-                return {
-                    ...event,
-                    title: newName,
-                };
+    const fetchTeams = async () => {
+        try {
+            const token = localStorage.getItem('userToken');
+            if (!token) {
+                console.error("Token JWT não encontrado");
+                return;
             }
-            return event;
-        });
-        setEvents(updated_Events);
+            const response = await axios.get('http://localhost:5000/api/teams', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            setTeams(response.data);
+        } catch (error) {
+            console.error("Erro ao obter equipes:", error.response ? error.response.data : error.message);
+        }
     };
 
-    const Delete_Event_Fun = (eventId) => {
-        const updated_Events = events.filter((event) => event.id !== eventId);
-        setEvents(updated_Events);
+    const formatEvents = (events) => {
+        const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+        const formatted = events.map(userEvents => {
+            const days = Array(daysInMonth).fill('');
+            userEvents.ferias.forEach(event => {
+                const date = new Date(event.data);
+                if (date.getMonth() === currentMonth && date.getFullYear() === currentYear) {
+                    days[date.getDate() - 1] = event.estado === 1 ? 'F-P' : 'F-A';
+                }
+            });
+            userEvents.ausencias.forEach(event => {
+                const date = new Date(event.data);
+                if (date.getMonth() === currentMonth && date.getFullYear() === currentYear) {
+                    days[date.getDate() - 1] = event.estado === 1 ? 'A-P' : 'A-A';
+                }
+            });
+            userEvents.presenciais.forEach(event => {
+                const date = new Date(event.data);
+                if (date.getMonth() === currentMonth && date.getFullYear() === currentYear) {
+                    days[date.getDate() - 1] = event.estado === 1 ? 'P-P' : 'P-A';
+                }
+            });
+            return { user: userEvents.user, days };
+        });
+        setFormattedEvents(formatted);
     };
+
+    const handleMonthChange = (direction) => {
+        let newDate;
+        if (direction === 'prev') {
+            newDate = new Date(currentYear, currentMonth - 1);
+        } else {
+            newDate = new Date(currentYear, currentMonth + 1);
+        }
+        setCurrentMonth(newDate.getMonth());
+        setCurrentYear(newDate.getFullYear());
+        setSelectedDate(newDate);
+    };
+
+    const handleTeamChange = (event) => {
+        setSelectedTeam(event.target.value);
+    };
+
+    const generateCSVReport = async (type) => {
+        let reportData = [];
+        if (type === 'monthly') {
+            reportData = formattedEvents.map(userEvents => {
+                return {
+                    user: `${userEvents.user.primeironome} ${userEvents.user.segundonome}`,
+                    ...userEvents.days.reduce((acc, day, index) => {
+                        acc[`Dia ${index + 1}`] = day;
+                        return acc;
+                    }, {})
+                };
+            });
+        } else if (type === 'yearly') {
+            try {
+                const token = localStorage.getItem('userToken');
+                if (!token) {
+                    console.error("Token JWT não encontrado");
+                    return;
+                }
+                const response = await axios.get('http://localhost:5000/api/all_users_events', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                const filteredEvents = selectedTeam ? response.data.filter(event => event.user.idequipa === selectedTeam) : response.data;
+                const yearlyEvents = filteredEvents.map(userEvents => {
+                    const months = Array(12).fill('').map(() => Array(31).fill(''));
+                    userEvents.ferias.forEach(event => {
+                        const date = new Date(event.data);
+                        if (date.getFullYear() === currentYear) {
+                            months[date.getMonth()][date.getDate() - 1] = event.estado === 1 ? 'F-P' : 'F-A';
+                        }
+                    });
+                    userEvents.ausencias.forEach(event => {
+                        const date = new Date(event.data);
+                        if (date.getFullYear() === currentYear) {
+                            months[date.getMonth()][date.getDate() - 1] = event.estado === 1 ? 'A-P' : 'A-A';
+                        }
+                    });
+                    userEvents.presenciais.forEach(event => {
+                        const date = new Date(event.data);
+                        if (date.getFullYear() === currentYear) {
+                            months[date.getMonth()][date.getDate() - 1] = event.estado === 1 ? 'P-P' : 'P-A';
+                        }
+                    });
+                    return { user: userEvents.user, months };
+                });
+
+                reportData = yearlyEvents.map(userEvents => {
+                    const userRow = { user: `${userEvents.user.primeironome} ${userEvents.user.segundonome}` };
+                    userEvents.months.forEach((month, monthIndex) => {
+                        month.forEach((day, dayIndex) => {
+                            const date = new Date(currentYear, monthIndex, dayIndex + 1);
+                            userRow[`${String(dayIndex + 1).padStart(2, '0')}/${String(monthIndex + 1).padStart(2, '0')}`] = day;
+                        });
+                    });
+                    return userRow;
+                });
+            } catch (error) {
+                console.error("Erro ao obter eventos anuais de todos os usuários:", error.response ? error.response.data : error.message);
+                return;
+            }
+        }
+
+        // Utilize ponto e vírgula como delimitador para compatibilidade com Excel em diferentes configurações regionais
+        const csvContent = [
+            ["Nome do Colaborador", ...Array.from({ length: type === 'monthly' ? new Date(currentYear, currentMonth + 1, 0).getDate() : 12 * 31 }, (_, i) =>
+                type === 'monthly' ? `${String(i + 1).padStart(2, '0')}/${String(currentMonth + 1).padStart(2, '0')}` : `${String(i % 31 + 1).padStart(2, '0')}/${String(Math.floor(i / 31) + 1).padStart(2, '0')}`)],
+            ...reportData.map(row => [row.user, ...Object.values(row).slice(1)])
+        ];
+
+        const csvString = csvContent.map(e => e.join(";")).join("\n");
+        const fileName = type === 'monthly' ? `Relatorio_${selectedDate.toLocaleDateString('pt-BR', { month: 'long' })}_${currentYear}.csv` : `Relatório_${currentYear}.csv`;
+        const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;' });
+        const link = document.createElement("a");
+        const url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", fileName);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <div className="main-container">
-            <div>
-                <NavbarRH />
-                <div className="container-RH">
-                    <div className="calendar-container">
-                        <Calendar
-                            value={selectedDate}
-                            onClickDay={Date_Click_Fun}
-                            tileClassName={({ date }) =>
-                                selectedDate &&
-                                date.toDateString() === selectedDate.toDateString()
-                                    ? "selected"
-                                    : events.some(
-                                        (event) =>
-                                            event.date.toDateString() ===
-                                            date.toDateString(),
-                                    )
-                                        ? "event-marked"
-                                        : ""
-                            }
-                        />{" "}
-                    </div>
-                    <div className="event-container">
-                        {" "}
-                        {selectedDate && (
-                            <div className="event-form">
-                                <h2> Create Event </h2>{" "}
-                                <p>
-                                    {" "}
-                                    Selected Date: {selectedDate.toDateString()}{" "}
-                                </p>{" "}
-                                <input
-                                    type="text"
-                                    placeholder="Event Name"
-                                    value={eventName}
-                                    onChange={Event_Data_Update}
-                                />{" "}
-                                <button
-                                    className="create-btn"
-                                    onClick={Create_Event_Fun}
-                                >
-                                    Click Here to Add Event{" "}
-                                </button>{" "}
-                            </div>
-                        )}
-                        {events.length > 0 && selectedDate && (
-                            <div className="event-list">
-                                <h2> My Created Event List </h2>{" "}
-                                <div className="event-cards">
-                                    {" "}
-                                    {events.map((event) =>
-                                        event.date.toDateString() ===
-                                        selectedDate.toDateString() ? (
-                                            <div
-                                                key={event.id}
-                                                className="event-card"
-                                            >
-                                                <div className="event-card-header">
-                                                <span className="event-date">
-                                                    {" "}
-                                                    {event.date.toDateString()}{" "}
-                                                </span>{" "}
-                                                    <div className="event-actions">
-                                                        <button
-                                                            className="update-btn"
-                                                            onClick={() =>
-                                                                Update_Event_Fun(
-                                                                    event.id,
-                                                                    prompt(
-                                                                        "ENTER NEW TITLE",
-                                                                    ),
-                                                                )
-                                                            }
-                                                        >
-                                                            Update Event{" "}
-                                                        </button>{" "}
-                                                        <button
-                                                            className="delete-btn"
-                                                            onClick={() =>
-                                                                Delete_Event_Fun(
-                                                                    event.id,
-                                                                )
-                                                            }
-                                                        >
-                                                            Delete Event{" "}
-                                                        </button>{" "}
-                                                    </div>{" "}
-                                                </div>{" "}
-                                                <div className="event-card-body">
-                                                    <p className="event-title">
-                                                        {" "}
-                                                        {event.title}{" "}
-                                                    </p>{" "}
-                                                </div>{" "}
-                                            </div>
-                                        ) : null,
-                                    )}{" "}
-                                </div>{" "}
-                            </div>
-                        )}{" "}
-                    </div>{" "}
-                </div>{" "}
+            <Navbarlogado />
+            <div className="container-RH">
+                <div className="month-selector">
+                    <button onClick={() => handleMonthChange('prev')}>Anterior</button>
+                    <span>{selectedDate.toLocaleDateString('pt-BR', { month: 'long', year: 'numeric' })}</span>
+                    <button onClick={() => handleMonthChange('next')}>Próximo</button>
+                </div>
+                <div className="team-selector">
+                    <label htmlFor="team">Selecionar Equipe: </label>
+                    <select id="team" value={selectedTeam} onChange={handleTeamChange}>
+                        <option value="">Todas as Equipes</option>
+                        {teams.map(team => (
+                            <option key={team.id} value={team.nome}>{team.nome}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="report-buttons">
+                    <button onClick={() => generateCSVReport('monthly')}>Gerar Relatório Mensal</button>
+                    <button onClick={() => generateCSVReport('yearly')}>Gerar Relatório Anual</button>
+                </div>
+                <div className="all-users-events">
+                    <h2>Eventos de Todos os Usuários</h2>
+                    <table className="events-table">
+                        <thead>
+                        <tr>
+                            <th>Nome do Colaborador</th>
+                            {Array.from({ length: new Date(currentYear, currentMonth + 1, 0).getDate() }, (_, i) => (
+                                <th key={i + 1}>{String(i + 1).padStart(2, '0')}/{String(currentMonth + 1).padStart(2, '0')}</th>
+                            ))}
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {formattedEvents.map((userEvents, index) => (
+                            <tr key={index}>
+                                <td>{userEvents.user.primeironome} {userEvents.user.segundonome}</td>
+                                {userEvents.days.map((day, i) => (
+                                    <td key={i} className={day}>{day}</td>
+                                ))}
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
